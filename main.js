91b825dd35f1062cf0b304544333b794f7453019
Dias <l1verpoolg24@gmail.com>
2022-10-28 04:08:09 +0600

    add new category

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 "use strict";
 
-const productName = "Samsung Galaxy S10";
-const amount = 7;
-const category = "smartphones";
-const price = 50000;
+{
+  const productName = "Samsung Galaxy S10";
+  const amount = 7;
+  const category = "smartphones";
+  const price = 50000;
+
+  let sum = amount * price;
+
+  console.log(productName);
+  console.log(sum);
+}
+
+{
+  const productName = "Apple Watch";
+  const amount = 5;
+  const category = "smartwatch";
+  const price = 30000;
+}
